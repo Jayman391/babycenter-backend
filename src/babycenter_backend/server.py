9d64c636479f57045fd5fb04196addd628b669eb
Jason Lobell <jasonlobell123@gmail.com>
2024-10-07 18:25:45 -0400

    refactor to use flask.request

--- src/babycenter_backend/server.py
+++ src/babycenter_backend/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import paramiko
 from flask_cors import CORS
-from handler import RequestHandler
+from babycenter_backend.handler import RequestHandler
 
 handler = RequestHandler()
 query_factory = handler.query_factory
@@ -11,61 +11,109 @@ user_factory = handler.user_factory
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes and origins
 
+# Refactored /query route to use request.args instead of URL parameters
+@app.route('/query', methods=['GET'])
+def query():
+    try:
+        # Extract query parameters from request.args
+        country = request.args.get('country')
+        format = request.args.get('format')
+        start = request.args.get('start')
+        end = request.args.get('end')
+        keywords = request.args.get('keywords', '').split(',')
+        groups = request.args.get('groups', '').split(',')
+        num_comments = request.args.get('num_comments', type=int)
+        post_or_comment = request.args.get('post_or_comment')
+        num_documents = request.args.get('num_documents', type=int)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+        # create user
+        user = user_factory.create_user()
+        user.query_created = True
+        handler.users[user.id] = user
 
-
-@app.route("/auth/<username>/<password>", methods=['GET'])
-def auth(username, password):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try :
-        ssh.connect('vacc-user1.uvm.edu', username=username, password=password)
-        return jsonify({"status": "success", "message": "Authentication successful"})
-    except Exception as e:
-        return jsonify({"status": "error", "message": e}) 
-        
-
-@app.route('/query/<country>/<format>/<start>/<end>/<keywords>/', methods=['GET'])
-def query(country, format, start, end, keywords):
-    #### PLACEHOLDER FOR QUERYING DATABASE ####
-    try :
-        return jsonify({"status": "success", "message": "Query successful", "content": {
+        # Construct params for handler
+        params = {
+            "request_type": "query",
+            "user_id": user.id,
             "country": country,
             "format": format,
             "start": start,
             "end": end,
-            "keywords": keywords
-        }})
-    except Exception as e:
-        return jsonify({"status": "error", "message": e}) 
-        
+            "keywords": keywords,
+            "groups": groups,
+            "num_comments": num_comments,
+            "post_or_comment": post_or_comment,
+            "num_documents": num_documents
+        }
 
-@app.route('/topic/<embedding>/<dimred>/<clustering>/<vectorizer>/', methods=['GET'])
-def topic(embedding, dimred, clustering, vectorizer, ):
-    #### PLACEHOLDER FOR TOPIC MODELING ####
-    try :
-        return jsonify({"status": "success", "message": "Topic modeling successful", "content": {
-            "embedding": embedding,
-            "dimred": dimred,
-            "clustering": clustering,
-            "vectorizer": vectorizer,
-        }})
+        response = handler.handle(params)
+        return jsonify({"user": user.id, "response": response})
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
+
+# Refactored /topic route to use request.args
+@app.route('/topic', methods=['GET'])
+def topic():
+    try:
+        # Extract query parameters from request.args
+        user_id = request.args.get('user_id')
+        embedding = request.args.get('embedding')
+        dimred = request.args.get('dimred')
+        clustering = request.args.get('clustering')
+        vectorizer = request.args.get('vectorizer')
+
+        return jsonify({
+            "status": "success",
+            "message": "Topic modeling successful",
+            "content": {
+                "embedding": embedding,
+                "dimred": dimred,
+                "clustering": clustering,
+                "vectorizer": vectorizer,
+            }
+        })
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
+
+# Refactored /ngram route to use request.args
+@app.route('/ngram', methods=['GET'])
+def ngram():
+    try:
+        # Extract query parameters from request.args
+        user_id = request.args.get('user_id')
+        country = request.args.get('country')
+        start = request.args.get('start')
+        end = request.args.get('end')
+        keywords = request.args.get('keywords', '').split(',')
+
+        return jsonify({
+            "status": "success",
+            "message": "Visualization successful",
+            "content": {
+                "country": country,
+                "start": start,
+                "end": end,
+                "keywords": keywords
+            }
+        })
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
+
+"""
+@app.route("/auth/<username>/<password>", methods=['GET'])
+def auth(username, password):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect('vacc-user1.uvm.edu', username=username, password=password)
+        return jsonify({"status": "success", "message": "Authentication successful"})
     except Exception as e:
         return jsonify({"status": "error", "message": e})
-    
-@app.route('/ngram/<country>/<start>/<end>/<keywords>/', methods=['GET'])
-def ngram(country, start, end, keywords):
-    #### PLACEHOLDER FOR VISUALIZATION ####
-    try :
-        return jsonify({"status": "success", "message": "Visualization successful", "content": {
-            "country": country,
-            "start": start,
-            "end": end,
-            "keywords": keywords
-        }})
-    except Exception as e:
-        return jsonify({"status": "error", "message": e})
-    
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True)
